a2361a36726e485d6cc344051bac362c21f8c062
eubule <eubulemalaba@gmail.com>
2018-10-12 17:35:21 +0300

    View products:Show products this page shows the products belonging to one category [Finishes #161111997]

---
+++ UI/javascript/original.js
@@ -0,0 +1,14 @@
+function validate() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email == "admin@store.com" && password == "admin") {
+        return true;
+    }
+    if (email == "admin@store.com") {
+        alert("Wrong Password for Admin User")
+        return false;
+    } else {
+        window.location = "login.html"
+        return false;
+    }
+}
